a630d3ad23f4714ca382f292b2f79387155e54a1
harry235day <763495653@qq.com>
2017-12-11 21:02:09 +0800

    第二次：滚动动态加载图片

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
 window.onload = function() {
+
     imgLocation("container", "box")
 
+    //拿到最后一张图片距离顶部的高度 
+    //与当前滚动距离顶部的值作比较
+    /**
+    	模拟json 假数据
+    */
+    var imageUrl = { "data": [{ "src": "image/1.png" }, { "src": "image/2.png" }, { "src": "image/3.png" }, { "src": "image/4.png" }, { "src": "image/5.png" }, { "src": "image/6.png" }, { "src": "image/7.png" }, { "src": "image/8.png" }, { "src": "image/9.png" }] };
+    window.onscroll = function() {
+        var flag = checkScrollFlag();
+        if (flag) {
 
+            var cparent = document.getElementById("container");
+            for (var i = 0; i < imageUrl.data.length; i++) {
+                /**
+    			<div class="box">
+            		<div class="box-img">
+                		<img src="image/1.png" alt="">
+            		</div>
+        		</div>
+    			**/
+                //创建标签 class 加入父容器
+                var ccontent = document.createElement("div");
+                ccontent.className="box";
+                cparent.appendChild(ccontent);
+
+                var boximg = document.createElement("div");
+                boximg.className="box-img";
+                ccontent.appendChild(boximg);
+
+                var image = document.createElement("img");
+                image.src=imageUrl.data[i].src;
+                boximg.appendChild(image);	
+            }
+
+            //重新走 瀑布流的逻辑
+            imgLocation("container", "box")
+
+        }
+    }
 
 
 }
-
+//判断是否加载
+function checkScrollFlag() {
+    var cparent = document.getElementById("container");
+    //得到所有子控件
+    var ccontent = getChildElement(cparent, "box");
+    // 得到最后一张图片距离顶部的高度
+    var lastContenHeight = ccontent[ccontent.length - 1].offsetTop;
+    // 得到滚动距离顶部的高度  浏览器兼容
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    //浏览器的高度
+    var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    // console.log(lastContenHeight+"-------"+scrollTop+"----"+clientHeight);
+    if (lastContenHeight < scrollTop + clientHeight) {
+        return true;
+    }
+}
 
 function imgLocation(parent, content) {
     //获取 当前的 父容器
